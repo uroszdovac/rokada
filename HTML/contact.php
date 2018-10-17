5ce8f9dccf847147f3e41e6b35b2585053fca2da
BIT Student <classroom@bgit.rs>
2018-10-17 16:35:35 +0200

    php

--- HTML/contact.php
+++ HTML/contact.php
@@ -86,13 +86,13 @@
 		$email=$_POST['email'];
 		$msg=$_POST['msg'];
 
-		$to='urosmijatovic87@mail.com'; // Receiver Email ID, Replace with your email ID
+		$to='urosmijatovic87@gmail.com'; // Receiver Email ID, Replace with your email ID
 		$subject='Form Submission';
 		$message="Name :".$name."\n"."Wrote the following :"."\n\n".$msg;
 		$headers="From: ".$email;
 
 		if(mail($to, $subject, $message, $headers)){
-			echo "<h1>Sent Successfully! Thank you"." ".$name.", We will contact you shortly!</h1>";
+			echo "<h1 class=''>Sent Successfully! Thank you"." ".$name.", We will contact you shortly!</h1>";
 		}
 		else{
 			echo "Something went wrong!";
